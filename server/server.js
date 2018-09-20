a182610e823632a563d73bc1c339ff5e0b682b05
yoniblumenfeld <yonei06@gmail.com>
2018-09-20 13:53:32 +0300

    Added many functions

--- server/server.js
+++ server/server.js
@@ -9,6 +9,24 @@ let app = express();
 const port = process.argv[2] || 3000;
 
 app.use(bodyParser.json());
+
+//Get users
+app.get('/users', (req, res) => {
+    User.find().then((users) => {
+        res.send({ users });
+    });
+})
+
+//Get user
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id;
+    User.findById(id).then((user) => {
+        res.send({ user });
+    });
+});
+
+
+//Users add
 app.post('/users/add', (req, res) => {
     console.log(req.body);
     let user = new User(req.body)
@@ -40,6 +58,74 @@ app.post('/users/add', (req, res) => {
     })
 
 });
+
+//users Update
+app.post('/users/update/:id', (req, res) => {
+    let id = req.params.id;
+    let updatedFields = req.body;
+    User.findById(id).then((user) => {
+        user.set(updatedFields);
+        user.save().then((updatedUser) => {
+            res.send({ updatedUser, updated: true });
+        }, (err) => {
+            res.send({ err, updated: false })
+        })
+    }, (err) => {
+        res.send({ err, updated: false })
+    });
+});
+
+//Users delete
+app.post('/users/delete/:id', (req, res) => {
+    let id = req.params.id;
+    User.findByIdAndRemove(id).then((deletedUser) => {
+        res.send({ deletedUser, deleted: true });
+    }, (err) => {
+        res.send({ err, deleted: false })
+    })
+});
+
+
+//show friends
+app.get('/users/:id/friends', (req, res) => {
+    let id = req.params.id;
+    User.findById(id).then((user) => {
+        res.send({ id, friends: user.friends });
+    })
+})
+
+//accept friend
+app.post('/users/:id/friends/accept/:friendId')
+
+//Add Friend
+app.post('/users/:id/friends/add/:friendId', (req, res) => {
+    let id = req.params.id;
+    let friendId = req.params.friendId;
+    User.findById(id).then((user) => {
+        let currentFriends = user.friends;
+        for (friend of currentFriends) {
+            console.log('friend in',friend);
+            if (friend.friendId === friendId) {
+                res.send({ user, added: false, errorMsg: 'friend already exists!' })
+            }
+        }
+        console.log('current friends', currentFriends);
+        User.findById(friendId).then((friendUser) => {
+            currentFriends.push({ friendId, requestStatus: 'pending', canApprove: false })
+            user.set({ friends: currentFriends });
+            let friendFriends = friendUser.friends;
+            friendFriends.push({ id, requestStatus: 'pending', canApprove: true })
+            friendUser.set({ friends: friendFriends });
+            user.save().then((user) => {
+                friendUser.save().then((friendUser) => {
+                    res.send({ friendUser, user, added: true });
+                })
+
+            });
+        })
+    })
+})
+
 
 app.listen(port, () => {
     console.log('Starting app on port', port);
